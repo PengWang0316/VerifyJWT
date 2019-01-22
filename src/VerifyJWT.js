1510851e0b8e16bf18c02110389810712ba73e41
Kevin Wang <rocwang0316@gmail.com>
2019-01-22 10:56:06 -0800

    Added the VerifyJWT.js

---
+++ src/VerifyJWT.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import log from '@kevinwang0316/log';
+
+
+/** Verify and return user object from jwt message
+ * @param { string } message includes jwt message
+ * @param { string } jwtSecret contains secret key for JWT
+ * @return { object } return the user object that was verified by jsonwebtoken or a false when fails to verify
+ */
+const verifyJWT = (message, jwtSecret) => {
+  try {
+    return jwt.verify(message, jwtSecret.trim()); // For some reasons, the jwtSecret comes from EC2 parameter store has spaces
+  } catch (e) {
+    log.info(`Verify JWT failed with message ${message}`);
+    return false;
+  }
+};
+
+export default verifyJWT;
